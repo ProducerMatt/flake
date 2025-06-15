2d2974c4c1b50c26920746383ca2e0294bfec49f
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-06-15 12:41:50 -0500

    fix: actually use disko

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*";
     nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505";
     home-manager-stable.url = "https://flakehub.com/f/nix-community/home-manager/0.2505";
     home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
@@ -11,9 +11,13 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
+    disko.url = "github:nix-community/disko/latest";
+
     # DeterminateSystems nix branch with extra features
     nix-detsys.url = "https://flakehub.com/f/DeterminateSystems/nix-src/*";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+
+    # impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = { self, nixpkgs-stable, ... }@inputs:
@@ -33,6 +37,7 @@
         '';
         system.configurationRevision = flakeInfo.rev;
       });
+
     in {
       nixosConfigurations.newPortable = nixpkgs-stable.lib.nixosSystem {
         system = "x86_64-linux";
@@ -40,6 +45,8 @@
           flakeInfoModule
           inputs.determinate.nixosModules.default
           inputs.nix-index-database.nixosModules.nix-index
+          inputs.disko.nixosModules.disko
+          # inputs.impermanence.nixosModules.impermanence
           ./nixos/newPortable/configuration.nix
         ];
       };
