8b35dd281c3581e485235319e5dec62880ec596c
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-07-21 21:18:34 -0500

    feat(newPortable): APC UPS monitoring

--- nixos/newPortable/default.nix
+++ nixos/newPortable/default.nix
@@ -137,6 +137,14 @@ in {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  power.ups = {
+    enable = true;
+    ups."APCBackUPS1500" = {
+      driver = "apcupsd";
+    };
+  };
+  services.apcupsd.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
