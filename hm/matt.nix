2bc382a37c33657c80f8f21a09ca6d82355daa4c
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-07-28 19:34:08 -0500

    fix: unneeded variable

--- hm/matt.nix
+++ hm/matt.nix
@@ -25,7 +25,6 @@
   programs.gpg.enable = true;
 
   home.sessionVariables = {
-    FLAKE = self.flakeInfo.rootDir;
     REALNAME = "ProducerMatt";
     EMAIL = "ProducerMatt42@gmail.com";
     KEYID = "E6EA80E5CB3E1F9C";
