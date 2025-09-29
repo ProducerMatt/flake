a1ebb99b4f58e2711c7cf3a353d6cd004c5929e3
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-09-29 10:02:42 -0500

    feat: disable diffoscope, broken

--- pkg-lists/dev.nix
+++ pkg-lists/dev.nix
@@ -18,7 +18,7 @@ with pkgs; [
   resolve-march-native # find appropriate compiler flags for your cpu
   remarshal # convert between config file formats
   rtx # https://github.com/jdxcode/rtx
-  diffoscope # extensive diff tool -- directories, PDF files, ISOs, etc
+  #diffoscope # extensive diff tool -- directories, PDF files, ISOs, etc
 
   #ansible
   #ansible-lint
