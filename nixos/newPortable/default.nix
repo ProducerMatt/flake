953e7211a6415ec3fdf56da4b4216c08a7ff6bed
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-11-17 09:06:42 -0600

    Revert "fix(mattsDesktop): tearing and stuttering USB audio"
    This reverts commit b99882eee46a9d3cef81d528aebbefeb509f37f6.

--- nixos/newPortable/default.nix
+++ nixos/newPortable/default.nix
@@ -76,51 +76,6 @@ in {
 
   nixpkgs.config.allowUnfree = true;
 
-  # stuttering with intel c-states in USB audio
-  services.pipewire.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/disable-suspension.conf" ''
-        monitor.alsa.rules = [
-        {
-          matches = [
-            {
-              # Matches all sources
-              node.name = "~alsa_input.*"
-            },
-            {
-              # Matches all sinks
-              node.name = "~alsa_output.*"
-            }
-          ]
-          actions = {
-            update-props = {
-              session.suspend-timeout-seconds = 0
-            }
-          }
-        }
-      ]
-      # bluetooth devices
-      monitor.bluez.rules = [
-        {
-          matches = [
-            {
-              # Matches all sources
-              node.name = "~bluez_input.*"
-            },
-            {
-              # Matches all sinks
-              node.name = "~bluez_output.*"
-            }
-          ]
-          actions = {
-            update-props = {
-              session.suspend-timeout-seconds = 0
-            }
-          }
-        }
-      ]
-    '')
-  ];
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
