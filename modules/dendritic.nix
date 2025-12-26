2303b82bcfba95d4f7f030ed54d8edd8dc48027e
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2025-12-25 18:51:58 -0600

    fix: improve flake-file

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -1,10 +1,19 @@
 {
   inputs,
   lib,
+  pkgs,
+  system,
   ...
 }: {
-  flake-file.inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
-  flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
+  flake-file = {
+    inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
+    inputs.den.url = lib.mkDefault "github:vic/den";
+
+    formatter = pkgs.alejandra;
+    nixConfig = {
+      nixpkgs = import ../pkg-options.nix {inherit system inputs;};
+    };
+  };
   imports = [
     (inputs.flake-file.flakeModules.dendritic or {})
     (inputs.den.flakeModules.dendritic or {})
---
+++ pkg-options.nix
@@ -0,0 +1,32 @@
+{
+  system,
+  inputs,
+}: let
+  config = {
+    allowUnfree = true;
+    checkMeta = true;
+    warnUndeclaredOptions = true;
+    # allow NixOS system config cross compilation
+    allowUnsupportedSystem = true;
+  };
+in {
+  inherit system config;
+  overlays = let
+    gimme = title: name: (_final: _prev: {
+      ${name} = inputs.${name}.packages.${system}.${title};
+    });
+  in [
+    (final: prev: (import pkgs/default.nix) final)
+    #(gimme "default" "nixpkgs-hammering")
+    #(gimme "nix-btm" "nix-btm")
+    inputs.nix-detsys.overlays.default
+    (gimme "nix" "nix-detsys")
+    (gimme "nil" "nil")
+    (gimme "nixd" "nixd")
+
+    inputs.emacs-overlay.overlays.default
+    (_: _: {
+      _unstable = import inputs.nixpkgs {inherit system config;};
+    })
+  ];
+}
