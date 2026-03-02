0333ec24986ab954350f8ee366cda4c436bdd5c7
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2026-03-01 23:49:27 -0600

    feat: hm global pkgs

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -16,6 +16,9 @@
     (inputs.flake-parts.flakeModules.modules or {})
   ];
 
+  # NixOS hosts with home-manager have global packages
+  den.ctx.hm-host.nixos.home-manager.useGlobalPkgs = true;
+
   # this line enables den angle brackets syntax in modules.
   # in pure eval, __findFile must be imported in the flake module args to take effect
   _module.args.__findFile = den.lib.__findFile;
